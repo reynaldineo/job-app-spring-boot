79161bdc5c516b0c9d65271f2423c37b8da4033a
reynaldi neo <reynaldineoramadhani@gmail.com>
2025-05-31 00:39:39 +0700

    feat: init spring boot project

---
+++ src/test/java/com/reynaldineo/jobapp/JobappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reynaldineo.jobapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
